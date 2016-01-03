33be9bc1decaae236ed022e25b621ee27d12407b
Nate Harada <nharada1@gmail.com>
2016-01-03 14:14:33 -0500

    First commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn rest_server:rest_stub_app --chdir reststub --log-file -
+web: gunicorn rest_server:rest_stub_app --chdir fluxmon --log-file -
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='reststub',
+    name='fluxmon',
     packages=find_packages(exclude=['scripts', 'tests']),
     version='0.01')
